e92c41e3c94b8ee30a6d09bae5137efcab5a1f9d
Viet Huynh <xa.lach@yahoo.com>
2024-01-26 05:55:00 +0700

    change CSS to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css'
-
 import { useState } from 'react'
 
 import viteLogo from '/vite.svg'
@@ -11,24 +9,35 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex justify-around'>
         <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          <img
+            src={viteLogo}
+            className='h-24 p-2 will-change-[filter] transition-[filter_300ms] hover:drop-shadow-[0_0_2em_#646cffaa]'
+            alt='Vite logo'
+          />
         </a>
         <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+          <img
+            src={reactLogo}
+            className='h-24 p-2 will-change-[filter] transition-[filter_300ms] hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_infinite_20s_linear] motion-reduce:animate-none'
+            alt='React logo'
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className='font-bold text-5xl'>Vite + React</h1>
+      <div className='p-8'>
+        <button
+          className='bg-[#1a1a1a] border border-transparent rounded px-3 py-2 font-medium transition-colors hover:border-[#646cff] focus:outline-[4px_auto_-webkit-focus-ring-color] focus:visible:outline-[4px_auto_-webkit-focus-ring-color]'
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
+      <p className='text-[#888]'>
         Click on the Vite and React logos to learn more
       </p>
     </>
