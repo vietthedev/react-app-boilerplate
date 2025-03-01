015957c97c1f1d92d31a8e4b74987de78dd52697
Việt Huỳnh <vietthedev@gmail.com>
2025-03-01 08:24:29 +0000

    build: remove tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-import typography from "@tailwindcss/typography";
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [typography, daisyui],
+	plugins: [],
 };
